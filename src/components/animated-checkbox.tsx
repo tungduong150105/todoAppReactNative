acbb58925a7dd3c4a0fa575869a5f99ff2202923
tungduong150105 <tungduong150105@gmail.com>
2025-01-16 22:21:23 +0700

    ... Task label

--- src/components/animated-checkbox.tsx
+++ src/components/animated-checkbox.tsx
@@ -8,12 +8,14 @@ import Animated, {
   withSequence
 } from 'react-native-reanimated'
 import Svg, { Path, Defs, ClipPath, G } from 'react-native-svg'
+import AnimatedStroke from './animated-stroke'
 
 const MARGIN = 10
 const vWidth = 64 + MARGIN
 const vHeight = 64 + MARGIN
 
-const checkMarkPath = 'M8 32.5C18 39 26 47 26 47C26 47 33 28 63.5 4'
+const checkMarkPath =
+  'M15 31.1977C23.1081 36.4884 29.5946 43 29.5946 43C29.5946 43 37.5 25.5 69 1.5'
 const outLineBoxPath =
   'M24 0.5H40C48.5809 0.5 54.4147 2.18067 58.117 5.88299C61.8193 9.58532 63.5 15.4191 63.5 24V40C63.5 48.5809 61.8193 54.4147 58.117 58.117C54.4147 61.8193 48.5809 63.5 40 63.5H24C15.4191 63.5 9.58532 61.8193 5.88299 58.117C2.18067 54.4147 0.5 48.5809 0.5 40V24C0.5 15.4191 2.18067 9.58532 5.88299 5.88299C9.58532 2.18067 15.4191 0.5 24 0.5Z'
 
@@ -21,13 +23,13 @@ const AnimatedPath = Animated.createAnimatedComponent(Path)
 
 interface Props {
   checked?: boolean
+  highlightColor: string
+  checkMarkColor: string
+  boxOutLineColor: string
 }
 
 const AnimatedCheckBox = (props: Props) => {
-  const { checked } = props
-  const checkMarkColor = '#000000'
-  const highlightColor = '#ff0000'
-  const boxOutLineColor = '#000000'
+  const { checked, checkMarkColor, highlightColor, boxOutLineColor } = props
 
   const progress = useSharedValue(0)
 
@@ -57,6 +59,27 @@ const AnimatedCheckBox = (props: Props) => {
     <Svg
       viewBox={[-MARGIN, -MARGIN, vWidth + MARGIN, vHeight + MARGIN].join(' ')}
     >
+      <Defs>
+        <ClipPath id="clipPath">
+          <Path
+            fill="white"
+            stroke="gray"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d={outLineBoxPath}
+          />
+        </ClipPath>
+      </Defs>
+      <AnimatedStroke
+        progress={progress}
+        d={checkMarkPath}
+        stroke={highlightColor}
+        strokeWidth={5}
+        strokeLinejoin="round"
+        strokeLinecap="round"
+        strokeOpacity={checked || false ? 1 : 0}
+        fill="transparent"
+      />
       <AnimatedPath
         d={outLineBoxPath}
         strokeWidth={5}
@@ -64,7 +87,18 @@ const AnimatedCheckBox = (props: Props) => {
         strokeLinecap="round"
         animatedProps={animatedBoxProps}
       />
-      <Path d={checkMarkPath} stroke="black" fill="transparent" />
+      <G clipPath="url(#clipPath)">
+        <AnimatedStroke
+          progress={progress}
+          d={checkMarkPath}
+          stroke={checkMarkColor}
+          strokeWidth={5}
+          strokeLinejoin="round"
+          strokeLinecap="round"
+          strokeOpacity={checked || false ? 1 : 0}
+          fill="transparent"
+        />
+      </G>
     </Svg>
   )
 }
